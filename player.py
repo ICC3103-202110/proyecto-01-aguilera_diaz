a868f299b35f1359753325c2459263ad09c7f7f7
Felipe Aguilera <pipo@MacBook-Air-de-Felipe-3.local>
2021-04-09 10:09:59 -0400

    Se añade clase duque

--- player.py
+++ player.py
@@ -1,3 +1,4 @@
+import random
 
 class Player():
     def __init__(self, cards, name):
@@ -8,4 +9,24 @@ class Player():
     def __str__(self):
         return f"{self.name}: monedas: {self.coins}"
    
-        
+class Duke():
+    def __init__(self):
+         self.name = "duke"
+
+    def __str__(self):
+        return "duke"
+
+    def __repr__(self):
+        return "duke"    
+
+    def accion(self, player):
+        player.coins += 3
+        print("You added 3 coins")
+        print(player)   
+cards = []
+while True:
+    for i in range(1):
+        duke = Duke()
+        cards.append(duke)
+    print(cards)
+    break
