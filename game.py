b43289ed529864c8aac137585664039895512e1c
Felipe Aguilera <pipo@MacBook-Air-de-Felipe-3.local>
2021-04-21 15:34:26 -0400

    se añade el dar cartas a jugadores

--- game.py
+++ game.py
@@ -23,3 +23,36 @@ class Game():
             self.cards.append(countess)
             self.cards.append(captain)
         random.shuffle(self.cards)
+        
+    def give_cards(self):
+        for player in self.players*2:
+            index = random.randint(0, (len(self.cards) - 1))
+            card = self.cards.pop(index)
+            player.cards.append(card)
+            
+        for player in self.players:
+            print(f"{player.name}: {player.cards}")
+            input("next turn (press enter)")
+    
+    def play_cards(self, player):
+            print("0: duke")
+            print("1: assassin")
+            print("2: captain")
+            print("3: ambassador")
+            card = int(input("What card you want to play? "))
+    
+            if card == 0: 
+                actcard = "duke"
+                duke = Duke()
+                listcardsplayer = list(player.cards)
+                deck = self.cards
+                challengep1 = Challenge()
+                challengep1.action(player, self.players, actcard, deck)
+                if range(len(listcardsplayer)) == range(len(player.cards)): #condition to see if player has the same cards before the challenge
+                    answer = duke.action(player)
+                else:
+                    print(f"{player.name} you lost your turn")
+                    player.influence = len(player.cards)
+                    if player.influence == 0:
+                        self.players.remove(player)
+                    answer = True
