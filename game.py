0a654bd4dbc466f427457a6b9750a4065cc4553c
Felipe Aguilera <pipo@MacBook-Air-de-Felipe-3.local>
2021-04-21 15:35:50 -0400

    se añaden condiciones para jugar

--- game.py
+++ game.py
@@ -23,7 +23,7 @@ class Game():
             self.cards.append(countess)
             self.cards.append(captain)
         random.shuffle(self.cards)
-        
+
     def give_cards(self):
         for player in self.players*2:
             index = random.randint(0, (len(self.cards) - 1))
@@ -56,3 +56,104 @@ class Game():
                     if player.influence == 0:
                         self.players.remove(player)
                     answer = True
+            if card == 1:
+                actcard = "assassin"
+                assassin = Assassin()
+                listcardsplayer = list(player.cards)
+                deck = self.cards
+
+                challengep1 = Challenge()
+                challenge = challengep1.action(player, self.players, actcard, deck)
+                
+                if challenge == "lost challenge":
+                    print(f"{player.name} you lost your turn")
+                    player.influence = len(player.cards)
+                    if player.influence == 0:
+                        self.players.remove(player)
+                    answer = True
+                
+                counterattackp1 = Counterattack()
+                cha = counterattackp1.action(player, self.players, actcard, deck)
+
+                if cha == "has the card":
+                    print(f"{player.name} you lost your turn, they blocked your foreign aid")
+                    answer = True
+                else:
+                    print("Sucess, they didn´t block you")
+                    if range(len(listcardsplayer)) == range(len(player.cards)):
+                        print("In what player do you want to use the card assassin: ")
+                        opcions = []
+                        for index, j in enumerate(self.players):
+                            if j != player:
+                                print(f"{index}. {j}")
+                                opcions.append(index)
+                        print()
+                        player_index = int(input("What player you want to assassin? "))
+                        targassassin = self.players[player_index]
+                        assassin = Assassin()
+                        answer = assassin.action(player, targassassin)
+                    else:
+                        print(f"{player.name} you lost your turn")
+                        player.influence = len(player.cards)
+                        if player.influence == 0:
+                            self.players.remove(player)
+                        answer = True
+            
+            if card == 2:
+                actcard = "captain"
+                captain = Captain()
+                listcardsplayer = list(player.cards)
+                deck = self.cards
+
+                challengep1 = Challenge()
+                challenge = challengep1.action(player, self.players, actcard, deck)
+
+                if challenge == "lost challenge":
+                    print(f"{player.name} you lost your turn")
+                    player.influence = len(player.cards)
+                    if player.influence == 0:
+                        self.players.remove(player)
+                    answer = True
+
+                counterattackp1 = Counterattack()
+                cha = counterattackp1.action(player, self.players, actcard, deck) 
+
+                if cha == "has the card":
+                    print(f"{player.name} you lost your turn, they blocked your foreign aid")
+                    answer = True
+                else:
+                    print("Sucess, they didn´t block ")
+                    if range(len(listcardsplayer)) == range(len(player.cards)):
+                        print("To what player do you want to use the card: ")
+                        opcions = []
+                        for index, j in enumerate(self.players):
+                            if j != player:
+                                print(f"{index}. {j}")
+                                opcions.append(index)
+                        print()
+                        player_index = int(input("What player you want to steal? "))
+                        targcaptain = self.players[player_index]
+                        captain = Captain()
+                        answer = captain.action(player, targcaptain)
+                    else:
+                        print(f"{player.name} you lost your turn")
+                        player.influence = len(player.cards)
+                        if player.influence == 0:
+                            self.players.remove(player)
+                        answer = True
+            if card == 3:
+                actcard = "ambassador"
+                ambassador = Ambassador()
+                listcardsplayer = list(player.cards)
+                deck = self.cards
+                challengep1 = Challenge()
+                challengep1.action(player, self.players, actcard, deck)
+                if range(len(listcardsplayer)) == range(len(player.cards)):
+                    answer = ambassador.action(player, deck)
+                else:
+                    print(f"{player.name} you lost your turn")
+                    player.influence = len(player.cards)
+                    if player.influence == 0:
+                        self.players.remove(player)
+                    answer = True
+            return answer
