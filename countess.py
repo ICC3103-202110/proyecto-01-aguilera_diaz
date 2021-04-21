f640135ed8834efa4786781a0dfa6897d85b473f
Felipe Aguilera <pipo@MacBook-Air-de-Felipe-3.local>
2021-04-21 15:27:46 -0400

    se crea contessa

--- countess.py
+++ countess.py
@@ -1,1 +1,9 @@
+class Countess():
+    def __init__(self):
+         self.name = "countess"
 
+    def __str__(self):
+        return "countess"
+
+    def __repr__(self):
+        return "countess"
